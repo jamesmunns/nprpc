cf1019627d26c18ff0040883f276de4017c56ef5
James Munns <james.munns@oxidecomputer.com>
2026-09-05 11:53:36 +0200

    Switch to schema-ng, fill in some composite things

--- nprpc/src/lib.rs
+++ nprpc/src/lib.rs
@@ -2,26 +2,40 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use postcard_schema::{Schema, key::Key};
+use postcard_schema_ng::{Schema, key::Key};
 use serde::{Deserialize, Serialize};
 
 /////////////////////////////////////////////////////////
 // EXAMPLES
 /////////////////////////////////////////////////////////
 
+// TODO: we could dedupe merge_keylists and merge_schemalists by making a macro
+// that looks like:
+//
+// ```rust
+// merge_lists! {
+//      Key,                        // Type
+//      Key::from_bytes([0u8; 8]),  // Initial const value expr
+//      [
+//          $(...)*                 // Array expansion goes here
+//      ]
+// }
+// ```
+
 /// Internal macro for merging generated keylists, used for
 /// `compose_interfaces!`
 #[macro_export]
 macro_rules! merge_keylists {
     ($($($segment:ident)::+$(,)?)*) => {{
-        const TOTAL_LEN: usize = $crate::count_keys(
+        use postcard_schema_ng::key::Key;
+        const TOTAL_LEN: usize = $crate::count_nested::<Key>(
             &[$(
                 $($segment)::+,
             )*]
         );
-        const ALL_KEYS_MERGED: [postcard_schema::key::Key; TOTAL_LEN] = const {
-            const ONE: postcard_schema::key::Key = unsafe {
-                postcard_schema::key::Key::from_bytes([0u8; 8])
+        const ALL_KEYS_MERGED: [Key; TOTAL_LEN] = const {
+            const ONE: Key = {
+                Key::from_bytes([0u8; 8])
             };
             let mut arr = [ONE; TOTAL_LEN];
             let mut idx_arr = 0;
@@ -36,12 +50,43 @@ macro_rules! merge_keylists {
             assert!(idx_arr == TOTAL_LEN);
             arr
         };
-        const SLI: &[postcard_schema::key::Key] = &ALL_KEYS_MERGED;
+        const SLI: &[Key] = &ALL_KEYS_MERGED;
         SLI
     }};
 }
 
-/// ```rust
+/// Internal macro for merging generated schemalists, used for
+/// `compose_interfaces!`
+#[macro_export]
+macro_rules! merge_schemalists {
+    ($($($segment:ident)::+$(,)?)*) => {{
+        use postcard_schema_ng::schema::DataModelType;
+        const TOTAL_LEN: usize = $crate::count_nested::<&DataModelType>(
+            &[$(
+                $($segment)::+,
+            )*]
+        );
+        const ALL_SCHEMAS_MERGED: [&DataModelType; TOTAL_LEN] = const {
+            const ONE: &DataModelType = &DataModelType::Unit;
+            let mut arr = [ONE; TOTAL_LEN];
+            let mut idx_arr = 0;
+            $(
+                let mut chidx = 0;
+                while chidx < $($segment)::+.len() {
+                    arr[idx_arr] = $($segment)::+[chidx];
+                    chidx += 1;
+                    idx_arr += 1;
+                }
+            )*
+            assert!(idx_arr == TOTAL_LEN);
+            arr
+        };
+        const SLI: &[&DataModelType] = &ALL_SCHEMAS_MERGED;
+        SLI
+    }};
+}
+
+/// ```rust,ignore
 /// interface! {
 ///      mod: ops,
 ///      | method            | request       | response      |
@@ -61,16 +106,38 @@ macro_rules! interface {
         pub mod $mod_name {
             use super::*;
 
+            pub mod schemas {
+                #[allow(unused_imports)]
+                use super::*;
+                use postcard_schema_ng::schema::DataModelType;
+
+                // All schemas, un-deduplicated
+                pub const ALL_REQ_SCHEMAS: &[&DataModelType] = &[
+                    $(
+                        $req_ty::SCHEMA,
+                    )*
+                ];
+                pub const ALL_RESP_SCHEMAS: &[&DataModelType] = &[
+                    $(
+                        $resp_ty::SCHEMA,
+                    )*
+                ];
+            }
+
             /// Calculated keys for all methods in this interface
             pub mod keys {
                 #[allow(unused_imports)]
                 use super::*;
+                use postcard_schema_ng::key::Key;
+
                 $(
                     #[allow(non_upper_case_globals)]
-                    pub const $mthd: postcard_schema::key::Key = $crate::endpoint_key2::<$req_ty, $resp_ty>(stringify!($mthd));
+                    pub const $mthd: Key = $crate::endpoint_key2::<$req_ty, $resp_ty>(
+                        stringify!($mthd)
+                    );
                 )*
 
-                pub const ALL_KEYS: &[postcard_schema::key::Key] = &[
+                pub const ALL_KEYS: &[Key] = &[
                     $(
                         $mthd,
                     )*
@@ -117,11 +184,11 @@ macro_rules! interface {
                     // basis.
                     match hdr.key {
                         $(
-                            keys::$mthd => $crate::process::<
+                            keys::$mthd => $crate::process_endpoint::<
                                 Self,
                                 $req_ty,
                                 $resp_ty,
-                            >(hdr, body, output, <Self as Server>::$mthd),
+                            >(self, hdr, body, output, <Self as Server>::$mthd),
                         )*
 
                         // None of the keys matched, return an error.
@@ -134,7 +201,7 @@ macro_rules! interface {
     };
 }
 
-/// ```rust
+/// ```rust,ignore
 /// compose_interfaces! {
 ///      mod: composite,
 ///      interfaces: [
@@ -153,6 +220,29 @@ macro_rules! compose_interfaces {
         ]
     ) => {
         pub mod $mod_name {
+            pub mod schemas {
+                use postcard_schema_ng::schema::DataModelType;
+                pub const ALL_REQ_SCHEMAS: &[&DataModelType] = $crate::merge_schemalists!(
+                    $(
+                        super::super::$intfc::schemas::ALL_REQ_SCHEMAS,
+                    )*
+                );
+                pub const ALL_RESP_SCHEMAS: &[&DataModelType] = $crate::merge_schemalists!(
+                    $(
+                        super::super::$intfc::schemas::ALL_RESP_SCHEMAS,
+                    )*
+                );
+            }
+
+            pub mod keys {
+                use postcard_schema_ng::key::Key;
+                pub const ALL_KEYS: &[Key] = $crate::merge_keylists!(
+                    $(
+                        super::super::$intfc::keys::ALL_KEYS,
+                    )*
+                );
+            }
+
             pub trait Server: $(super::$intfc::Server+)* {
                 fn process_one<'buf>(
                     &mut self,
@@ -174,12 +264,7 @@ macro_rules! compose_interfaces {
                 ) -> Result<&'buf mut [u8], $crate::Error> {
                     // Check all the merged keys to make sure that none of the composed
                     // endpoints have a collision
-                    const MERGED_KEYS: &[postcard_schema::key::Key] = $crate::merge_keylists!(
-                        $(
-                            super::$intfc::keys::ALL_KEYS,
-                        )*
-                    );
-                    const _: () = $crate::assert_unique(MERGED_KEYS);
+                    const _: () = $crate::assert_unique(keys::ALL_KEYS);
 
                     $(
                         if super::$intfc::keys::ALL_KEYS.contains(&hdr.key) {
@@ -193,40 +278,56 @@ macro_rules! compose_interfaces {
     };
 }
 
+pub enum Method {
+    Request,
+    Response,
+}
+
 pub struct Header {
-    key: Key,
+    pub method: Method,
+    pub version: u8,
+    pub seqno: u16,
+    pub key: Key,
 }
 pub struct Request<T> {
     pub hdr: Header,
     pub req: T,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Error {
+    WrongMethod,
     Unknown,
+    PostcardDeser(postcard::Error),
+    PostcardSer(postcard::Error),
 }
 
-pub fn process<'a, S: ?Sized, Q, R>(
-    _hdr: Header,
-    _body: &[u8],
-    _out: &'a mut [u8],
-    _apply: fn(&mut S, Request<Q>) -> R,
-) -> Result<&'a mut [u8], Error> {
-    // deserialize
-    // let req = Request::new(
-    //      hdr,
-    //      req: deser(_body)?,
-    // };
-    // let resp = apply(S, req);
-    // let used = ser(&resp)?;
-    // Ok(used)
-    todo!()
+pub fn process_endpoint<'out, 'de, S: ?Sized, Q, R>(
+    server: &mut S,
+    hdr: Header,
+    body: &'de [u8],
+    out: &'out mut [u8],
+    apply: fn(&mut S, Request<Q>) -> R,
+) -> Result<&'out mut [u8], Error>
+where
+    Q: Deserialize<'de>,
+    R: Serialize,
+{
+    // Deserialize
+    let body: Q = postcard::from_bytes(body).map_err(Error::PostcardDeser)?;
+    // Process request
+    let req = Request { hdr, req: body };
+    let resp = apply(server, req);
+    // Serialize response (TODO, we also need to do the header)
+    let used = postcard::to_slice(&resp, out).map_err(Error::PostcardSer)?;
+    Ok(used)
 }
 
 /////////////////////////////////////////////////////////
 // CONST HELPERS
 /////////////////////////////////////////////////////////
 
-pub const fn assert_impls_schema<T: postcard_schema::Schema>() {}
+pub const fn assert_impls_schema<T: postcard_schema_ng::Schema>() {}
 pub const fn assert_impl_serialize<T: serde::Serialize>() {}
 pub const fn assert_impl_deserialize<'d, T: serde::Deserialize<'d>>() {}
 pub const fn assert_unique(keys: &[Key]) {
@@ -282,14 +383,14 @@ pub const fn endpoint_key2<Q: Schema, R: Schema>(name: &str) -> Key {
         out[idx] = half_rsp[idx - 4];
         idx += 1;
     }
-    unsafe { Key::from_bytes(out) }
+    Key::from_bytes(out)
 }
 
-const fn count_keys(keys: &[&[Key]]) -> usize {
+const fn count_nested<T>(ts: &[&[T]]) -> usize {
     let mut idx = 0;
     let mut ct = 0;
-    while idx < keys.len() {
-        ct += keys[idx].len();
+    while idx < ts.len() {
+        ct += ts[idx].len();
         idx += 1;
     }
     ct
@@ -330,57 +431,6 @@ const fn count_keys(keys: &[&[Key]]) -> usize {
  *                  }
  *              }
  *              // whatever serialize an err here
- *      }
- * }
- *
- * // server interface looks like:
- * trait Server {
- *      // Wrapper type that asserts uniqueness and adds schema endpoints?
- *      const SCHEMAS: &'static [EndpointInfo] = ...;
- *      fn one_more_thing(&mut self, req: Request<u32>) -> bool;
- *      fn process_one<S: Server>(
- *           server: &mut S,
- *           hdr: Header,
- *           body: &[u8],
- *           output: &mut [u8],
- *      ) -> usize {
- *          // match style
- *          const one_more_thing: Key = Key::for<u32, bool>("one_more_thing");
- *          match hdr.key {
- *              one_more_thing => {
- *                  // TODO: make this an inner func that
- *                  let req: u32 = deser(body)?;
- *                  let req: Request<u32> = Request::new(&hdr, &req);
- *                  let resp = <S as Server>::one_more_thing(server, req)?;
- *                  serialize(output, &resp)?;
- *                  Ok(())
- *              }
- *          }
- *
- *          // search style
- *          fn handle<S: Server, Q: Deserialize, R: Serialize>(
- *              server: &mut S,
- *              hdr: Header,
- *              body: &[u8],
- *              output: &mut [u8],
- *              func: fn(&mut S, Request<Q>) -> Result<R, Error>,
- *          ) -> usize {
- *              let req: u32 = deser::<Q>(body)?;
- *              let req: Request<u32> = Request::new(&hdr, &req);
- *              let resp: R = <S as Server>::one_more_thing(server, req)?;
- *              let used = serialize(output, &resp)?;
- *              Ok(used)
- *          }
- *          const MAP: &[(Key, fn(&mut S, Header, body, output))] = &[
- *              (Key::for<u32, bool>("one_more_thing"), handle::<S, u32, bool>),
- *          ];
- *      }
- * }
- *
- * // In the server location, this is what the user actually implements
- * impl two::Server {
- *      fn one_more_thing(&mut self, req: Request<u32>) -> bool {
- *          // ...
  *      }
  * }
  *
@@ -469,6 +519,19 @@ pub struct Str<'a>(&'a str);
 #[derive(Schema, Deserialize, Serialize)]
 pub struct Str2<'a, 'b>(&'a str, &'b str);
 
+#[derive(Schema, Deserialize, Serialize)]
+pub struct Fancy {
+    a: u8,         // 1
+    b: u16,        // 3
+    c: u32,        // 5
+    d: u64,        // 10
+    e: (i8, i16),  // 4
+    f: (i32, i64), // 15
+    g: [bool; 8],  // 8
+                   // ==
+                   // 46 ✔️
+}
+
 #[cfg(not(feature = "std"))]
 interface! {
      mod: basic,
@@ -489,8 +552,9 @@ interface! {
      | mult_two   | u32     | u32          |
      | to_stringa | u32     | String       |
      | to_stringb | String  | u32          |
-     | billy      | String  | String       |
+     | billy      | Str<'a> | Str<'b>      |
      | to_stringd | String  | String       |
+     | is_good    | Fancy   | bool         |
 }
 
 interface! {
@@ -507,4 +571,118 @@ compose_interfaces! {
          two,
          ops,
      ]
+}
+
+pub struct ServerImpl;
+
+impl basic::Server for ServerImpl {
+    fn mult_two(&mut self, req: crate::Request<u32>) -> u32 {
+        req.req * 2
+    }
+
+    fn to_stringa(&mut self, req: crate::Request<u32>) -> String {
+        req.req.to_string()
+    }
+
+    fn to_stringb(&mut self, req: crate::Request<String>) -> u32 {
+        req.req.len() as u32
+    }
+
+    fn to_stringd(&mut self, req: crate::Request<String>) -> String {
+        req.req
+    }
+
+    fn billy<'a, 'b>(&mut self, req: crate::Request<Str<'a>>) -> Str<'b> {
+        if req.req.0.len() > 5 {
+            Str(":(")
+        } else {
+            Str(":)")
+        }
+    }
+
+    fn is_good(&mut self, req: crate::Request<Fancy>) -> bool {
+        let mut good = true;
+        for g in req.req.g {
+            good &= g;
+        }
+        good
+    }
+}
+
+impl ops::Server for ServerImpl {
+    fn mult_three(&mut self, req: crate::Request<u32>) -> u32 {
+        req.req * 3
+    }
+}
+
+impl two::Server for ServerImpl {
+    fn one_more_thing(&mut self, req: crate::Request<u32>) -> bool {
+        req.req.is_multiple_of(2)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    pub fn exercise() {
+        let mut x = ServerImpl;
+        let mut output = [0u8; 16];
+        let mut input = [0u8; 16];
+
+        let iused = postcard::to_slice(&200u32, &mut input).unwrap();
+        let resp = <ServerImpl as composite::Server>::process_one(
+            &mut x,
+            Header {
+                seqno: 123,
+                version: 0,
+                method: Method::Request,
+                key: endpoint_key2::<u32, u32>("mult_two"),
+            },
+            iused,
+            &mut output,
+        )
+        .unwrap();
+        let actually: u32 = postcard::from_bytes(resp).unwrap();
+        assert_eq!(actually, 400u32);
+    }
+
+    #[test]
+    pub fn exercise_borrowed() {
+        let mut x = ServerImpl;
+        let mut output = [0u8; 16];
+        let mut input = [0u8; 16];
+
+        let iused = postcard::to_slice("boop", &mut input).unwrap();
+        let resp = <ServerImpl as composite::Server>::process_one(
+            &mut x,
+            Header {
+                seqno: 124,
+                version: 0,
+                method: Method::Request,
+                key: endpoint_key2::<Str, Str>("billy"),
+            },
+            iused,
+            &mut output,
+        )
+        .unwrap();
+        let actually: Str<'_> = postcard::from_bytes(resp).unwrap();
+        assert_eq!(actually.0, ":)");
+    }
+
+    #[test]
+    pub fn sizes() {
+        for s in composite::schemas::ALL_REQ_SCHEMAS {
+            println!("{s:?} -> {:?}", s.max_size());
+        }
+        println!();
+        for s in composite::schemas::ALL_RESP_SCHEMAS {
+            println!("{s:?} -> {:?}", s.max_size());
+        }
+        println!();
+        for k in composite::keys::ALL_KEYS {
+            println!("{k:?}");
+        }
+        panic!();
+    }
 }
