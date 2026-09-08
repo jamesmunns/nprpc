27ffae2d969dca2df5c15516c56437dac5b9addf
James Munns <james.munns@oxidecomputer.com>
2026-09-08 14:23:07 +0200

    Clean up client and server demos a bit

--- demo/udp-client/src/main.rs
+++ demo/udp-client/src/main.rs
@@ -2,58 +2,16 @@ use std::{net::UdpSocket, num::Wrapping, time::Duration};
 
 use nprpc::{
     autobuffer,
-    io::client::{Backend, Interface},
+    io::{
+        Storage,
+        client::{Backend, Interface},
+    },
 };
 
 // TODO: compose clients
 use udp_api::hello::Client as _;
 use udp_api::kv::Client as _;
-
 autobuffer!(ApiBufs, udp_api::composite);
-
-// This could live in nprpc? Requires already connected I think, so maybe need
-// a wrapper type? Or we should use send_to?
-struct ConnectedUdpSocket(UdpSocket);
-
-impl Interface for ConnectedUdpSocket {
-    fn send_reply_raw<'a>(
-        &mut self,
-        outgoing: &[u8],
-        incoming: &'a mut [u8],
-    ) -> Result<&'a [u8], nprpc::Error> {
-        // TODO: these needs some kind of interface-specific error type
-        self.0.send(outgoing).unwrap();
-        let used = self.0.recv(incoming).unwrap();
-        Ok(&incoming[..used])
-    }
-}
-
-struct MyClient {
-    ctr: Wrapping<u16>,
-    buf: ApiBufs,
-    socket: ConnectedUdpSocket,
-}
-
-impl Backend for MyClient {
-    type Storage = ApiBufs;
-
-    type Interface = ConnectedUdpSocket;
-
-    fn next_sequence_number(&mut self) -> u16 {
-        let now = self.ctr;
-        self.ctr += 1;
-        now.0
-    }
-
-    fn parts(&mut self) -> (&mut Self::Storage, &mut Self::Interface) {
-        let Self {
-            ctr: _,
-            buf,
-            socket,
-        } = self;
-        (buf, socket)
-    }
-}
 
 fn main() {
     let socket = UdpSocket::bind("127.0.0.1:3400").expect("couldn't bind to address");
@@ -83,5 +41,50 @@ fn main() {
         let _ = client.set_name(&new_name).unwrap();
 
         std::thread::sleep(Duration::from_secs(1));
+    }
+}
+
+/////
+// TODO: Items below here should probably be in `nprpc` as standard interface
+// impls.
+//
+struct ConnectedUdpSocket(UdpSocket);
+
+impl Interface for ConnectedUdpSocket {
+    fn send_reply_raw<'a>(
+        &mut self,
+        outgoing: &[u8],
+        incoming: &'a mut [u8],
+    ) -> Result<&'a [u8], nprpc::Error> {
+        // TODO: these needs some kind of interface-specific error type
+        self.0.send(outgoing).unwrap();
+        let used = self.0.recv(incoming).unwrap();
+        Ok(&incoming[..used])
+    }
+}
+
+struct MyClient<S: Storage> {
+    ctr: Wrapping<u16>,
+    buf: S,
+    socket: ConnectedUdpSocket,
+}
+
+impl<S: Storage> Backend for MyClient<S> {
+    type Storage = S;
+    type Interface = ConnectedUdpSocket;
+
+    fn next_sequence_number(&mut self) -> u16 {
+        let now = self.ctr;
+        self.ctr += 1;
+        now.0
+    }
+
+    fn parts(&mut self) -> (&mut Self::Storage, &mut Self::Interface) {
+        let Self {
+            ctr: _,
+            buf,
+            socket,
+        } = self;
+        (buf, socket)
     }
 }
--- demo/udp-server/src/main.rs
+++ demo/udp-server/src/main.rs
@@ -3,22 +3,75 @@ use std::net::{SocketAddr, UdpSocket};
 use nprpc::{
     autobuffer,
     io::{
-        Storage, StorageView,
+        Storage,
         server::{Backend, RawInterfaceFrame},
     },
-    wire::Header,
 };
 use postcard_schema_ng::max_len::MaxLenString;
-use udp_api::composite::{self, Server};
+use udp_api::composite;
 
 const PORT: u16 = 5501;
 
 // Auto-sized buffer type
 autobuffer!(ApiBufs, udp_api::composite);
 
-struct Wire {
+struct ServerImpl {
+    name: Option<MaxLenString<32>>,
+}
+
+impl udp_api::hello::Server for ServerImpl {
+    fn loopback(&mut self, req: nprpc::Request<u32>) -> u32 {
+        println!("hello/loopback: {}", req.req);
+        req.req
+    }
+}
+
+impl udp_api::kv::Server for ServerImpl {
+    fn set_name(&mut self, req: nprpc::Request<MaxLenString<32>>) {
+        println!("kv/set_name: {}", req.req);
+        self.name = Some(req.req);
+    }
+
+    fn get_name(&mut self, _req: nprpc::Request<()>) -> Option<MaxLenString<32>> {
+        println!("kv/get_name");
+        self.name.clone()
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut wire = Wire {
+        socket: BoundUdpSocket(UdpSocket::bind(format!("127.0.0.1:{PORT}"))?),
+        buffers: ApiBufs::new(),
+    };
+    let mut server = ServerImpl { name: None };
+    println!("Server Buffer Sizes:");
+    println!("  - RQST: {}", wire.buffers.rqst_buf.len());
+    println!("  - RESP: {}", wire.buffers.resp_buf.len());
+    println!();
+
+    loop {
+        let res = wire.serve_one(|reqraw, outgoing| {
+            <ServerImpl as composite::Server>::process_one(
+                &mut server,
+                reqraw.hdr,
+                reqraw.rqst,
+                outgoing,
+            )
+        });
+        if let Err(e) = res {
+            println!("Err: {e:?}");
+        }
+    }
+}
+
+/////
+// TODO: Items below here should probably be in `nprpc` as standard interface
+// impls.
+//
+
+struct Wire<S: Storage> {
     socket: BoundUdpSocket,
-    buffers: ApiBufs,
+    buffers: S,
 }
 
 struct BoundUdpSocket(UdpSocket);
@@ -53,80 +106,12 @@ impl nprpc::io::server::Interface for BoundUdpSocket {
     }
 }
 
-impl nprpc::io::server::Backend for Wire {
-    type Storage = ApiBufs;
+impl<S: Storage> nprpc::io::server::Backend for Wire<S> {
+    type Storage = S;
     type Interface = BoundUdpSocket;
 
     fn parts(&mut self) -> (&mut Self::Storage, &mut Self::Interface) {
         let Self { socket, buffers } = self;
         (buffers, socket)
-    }
-}
-
-fn main() -> std::io::Result<()> {
-    let mut wire = Wire {
-        socket: BoundUdpSocket(UdpSocket::bind(format!("127.0.0.1:{PORT}"))?),
-        buffers: ApiBufs::new(),
-    };
-    let mut server = ServerImpl { name: None };
-    println!("Server Buffer Sizes:");
-    println!("  - RQST: {}", wire.buffers.rqst_buf.len());
-    println!("  - RESP: {}", wire.buffers.resp_buf.len());
-    println!();
-
-    loop {
-        let res = wire.serve_one(|reqraw, outgoing| {
-            <ServerImpl as composite::Server>::process_one(
-                &mut server,
-                reqraw.hdr,
-                reqraw.rqst,
-                outgoing,
-            )
-        });
-        // let StorageView { rqst_buf, resp_buf } = buffers.buffers();
-        // let Ok((got, peer)) = socket.recv_from(rqst_buf) else {
-        //     println!("Uhh (connect)...");
-        //     continue;
-        // };
-        // let used = &rqst_buf[..got];
-        // println!("==(RECV)=> {} ({:02X?})", got, used);
-        // // this is gross to *have* to do manually
-        // let Ok((hdr, remain)) = postcard::take_from_bytes::<Header>(used) else {
-        //     println!("Uhh (bad header)...");
-        //     // TODO: reply with error?
-        //     continue;
-        // };
-
-        // match server.process_one(hdr, remain, resp_buf) {
-        //     Ok(reply) => {
-        //         println!("<=(SEND)== {} ({:02X?})", reply.len(), reply);
-        //         println!();
-        //         socket.send_to(reply, peer).unwrap();
-        //     }
-        //     Err(e) => panic!("{e:?}"),
-        // }
-    }
-}
-
-struct ServerImpl {
-    name: Option<MaxLenString<32>>,
-}
-
-impl udp_api::hello::Server for ServerImpl {
-    fn loopback(&mut self, req: nprpc::Request<u32>) -> u32 {
-        println!("hello/loopback: {}", req.req);
-        req.req
-    }
-}
-
-impl udp_api::kv::Server for ServerImpl {
-    fn set_name(&mut self, req: nprpc::Request<MaxLenString<32>>) {
-        println!("kv/set_name: {}", req.req);
-        self.name = Some(req.req);
-    }
-
-    fn get_name(&mut self, _req: nprpc::Request<()>) -> Option<MaxLenString<32>> {
-        println!("kv/get_name");
-        self.name.clone()
     }
 }
